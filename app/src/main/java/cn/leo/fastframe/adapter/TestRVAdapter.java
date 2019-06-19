a7a6fd93999bc8e7fd663650ad90115bf1a34716
yjtx256@qq.com <yjtx256@qq.com>
2019-06-19 18:18:35 +0800

    更新logger

--- app/src/main/java/cn/leo/fastframe/adapter/TestRVAdapter.java
+++ app/src/main/java/cn/leo/fastframe/adapter/TestRVAdapter.java
@@ -1,8 +1,5 @@
 package cn.leo.fastframe.adapter;
 
-import android.view.View;
-import android.widget.Toast;
-
 import cn.leo.fastframe.R;
 import cn.leo.fastframe.bean.TestBean;
 import cn.leo.frame.base.AsyncRVAdapter;
@@ -19,15 +16,8 @@ public class TestRVAdapter extends AsyncRVAdapter<TestBean> {
     }
 
     @Override
-    protected void bindData(ItemHelper helper, final TestBean data) {
+    protected void bindData(ItemHelper helper, TestBean data) {
         helper.setText(R.id.tv_test, data.content);
-        helper.subscribeClick(R.id.tv_test);
     }
 
-    @Override
-    protected void onClickObserve(View view, TestBean data) {
-        if (view.getId() == R.id.tv_test) {
-            Toast.makeText(view.getContext(), "点击了：条目内 " + data.content, Toast.LENGTH_SHORT).show();
-        }
-    }
 }
